338029fb0b26112fe2f16484d9a3df0a984b20f1
Tony-Bustaty <bustatytony@gmail.com>
2026-05-08 18:37:20 +0300

    add recommendations suggested by CodeRabbitAi from Arcjet Docs for implmenation details

--- src/ws/server.ts
+++ src/ws/server.ts
@@ -29,21 +29,34 @@ export function attachWebSocketServer(server: http.Server) {
     path: "/ws",
     maxPayload: 1024 * 1024,
   });
-  wss.on("connection",async (socket,req) => {
+  server.on("upgrade",async(req,socket,head)=>{
     if(wsArcjet){
       try {
         const decision= await wsArcjet.protect(req);
         if(decision.isDenied()){
-          const code =decision.reason.isRateLimit()?1013:1008;
-          const reason=decision.reason.isRateLimit()?"Rate Limit Exceed":"Access denied";
-          socket.close(code,reason);
-          return;
-        }
+          if(decision.reason.isRateLimit()){
+
+            socket.write('HTTP/1.1 429 Too many Requests\r\n\r\n')
+          }else{
+            socket.write('HTTP/1.1 403 Forbidden\r\n\r\n');
+            socket.destroy();
+            return;
+          }
+        } 
       } catch (error) {
-        console.error("ws connection error",error)
-        socket.close(1011,'Server security error');
+        console.error("ws upgrade protection connection error",error)
+        socket.write('HTTP/1.1 500 Internal Server Error\r\n\r\n');
+           socket.destroy();
+            return;
       }
+
     }
+     wss.handleUpgrade(req, socket, head, (ws) => {
+            wss.emit('connection', ws, req);
+        });
+  })
+  wss.on("connection",async (socket,req) => {
+    
     SendJson(socket, { type: "welcome" });
     socket.on("error", console.error);
     socket.isAlive = true;
